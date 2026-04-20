98aa89327710f0de285ca949b82ede99e8a7b582
WAGTTZ <164768132+WAGTTZ@users.noreply.github.com>
2026-04-20 17:15:34 +0800

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// A 同學負責這部分
+void foo() {
+    
+}
+
+// B 同學負責這部分
+void bar() {
+    
+}
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
